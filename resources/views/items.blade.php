b0cd120831123d4cf85f27ea805f1c054483f189
nakano <nkkshushoku7021@gmail.com>
2022-04-24 10:24:11 +0900

    change item.blade.php

--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -1,29 +1,29 @@
-<!-- resources/views/books.blade.php -->
+<!-- resources/views/items.blade.php -->
 @extends('layouts.app')
 @section('content')
 
     <!-- Bootstrapの定形コード… -->
     <div class="card-body">
         <div class="card-title">
-            本のタイトル
+            作品のタイトル
         </div>
         
         <!-- バリデーションエラーの表示に使用-->
         @include('common.errors')
         <!-- バリデーションエラーの表示に使用-->
 
-        <!-- 本登録フォーム -->
+        <!-- 作品登録フォーム -->
         <form action="{{ url('items') }}" method="POST" class="form-horizontal">
             @csrf
 
-            <!-- 本のタイトル -->
+            <!-- 作品のタイトル -->
             <div class="form-group">
                 <div class="col-sm-6">
                     <input type="text" name="item_name" class="form-control">
                 </div>
             </div>
 
-            <!-- 本 登録ボタン -->
+            <!-- 作品 登録ボタン -->
             <div class="form-group">
                 <div class="col-sm-offset-3 col-sm-6">
                     <button type="submit" class="btn btn-primary">
@@ -33,6 +33,6 @@
             </div>
         </form>
     </div>
-    <!-- Book: 既に登録されてる本のリスト -->
+    <!-- Book: 既に登録されてる作品のリスト -->
 
 @endsection
