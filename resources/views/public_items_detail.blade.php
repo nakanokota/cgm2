a0c7322c24660d27ca85f41d3d7fa69427102ddb
nakano <nkkshushoku7021@gmail.com>
2022-04-25 18:22:54 +0900

    change public_items_view.blade.php

---
+++ resources/views/public_items_detail.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+    <div class="card-body">
+        <div class="card-body">
+            <div> <img src="../upload/img_{{$item->id}}.jpg" width="500"></div>
+        </div>
+    </div>
+@endsection
